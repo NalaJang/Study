322db2d9c59a493d0dac1b8872dbd10b1529fb3c
NalaJang <nala0897@naver.com>
2021-03-08 21:04:17 +0900

    3052번: 나머지

---
+++ Baekjoon/3052번-나머지.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class Main{
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		HashSet<Integer> h = new HashSet<>();
+		
+		for(int i = 0; i < 10; i++) {
+			h.add(Integer.parseInt(br.readLine()) % 42);
+		}
+		System.out.println(h.size());
+	}
+}
