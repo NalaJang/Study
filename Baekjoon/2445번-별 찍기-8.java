b59091edaa29c3640787a11b2f92554c53662730
NalaJang <nala0897@naver.com>
2021-03-27 00:00:44 +0900

    2445번: 별 찍기 - 8

---
+++ Baekjoon/2445번-별 찍기-8.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= i; j++) { sb.append("*"); }
+			
+			for(int j = 1; j <= (n-i)*2; j++) { sb.append(" "); }
+			
+			for(int j = 1; j <= i; j++) { sb.append("*"); }
+			
+			sb.append("\n");
+		}
+		
+		for(int i = 1; i < n; i++) {
+			for(int j = n-1; j >= i; j--) { sb.append("*"); }
+			
+			for(int j = 1; j <= i*2; j++) { sb.append(" "); }
+			
+			for(int j = n-1; j >= i; j--) { sb.append("*"); }
+			
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
